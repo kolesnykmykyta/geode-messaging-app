e2c469ee50ea2dab6ac5bb10976f97145e418313
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-03-29 17:55:56 +0200

    Create MAUI template

---
+++ Geode.Maui/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Geode.Maui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Geode.Maui/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Geode.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
