2f45fc607518dfe61e65dfbc773f3a931fc8e741
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-03-29 18:42:23 +0200

    Create API controllers endpoints template

--- Geode.API/Controllers/ChatController.cs
+++ Geode.API/Controllers/ChatController.cs
@@ -1,6 +1,46 @@
-﻿namespace Geode.API.Controllers
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Geode.API.Controllers
 {
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
     public class ChatController
     {
+        public ChatController()
+        {
+
+        }
+
+        public IActionResult GetUserChat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult JoinChat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult CreateChat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult ChangeChatName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult LeaveChat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IActionResult SendMessageInChat()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Geode.API/Controllers/UserController.cs
+++ Geode.API/Controllers/UserController.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Geode.API.Controllers
 {
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
     public class UserController : Controller
     {
+        public UserController()
+        {
+
+        }
+
+        public IActionResult UpdateUsername()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
