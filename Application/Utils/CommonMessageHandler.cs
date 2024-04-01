018925cbe9293c56b828a26d91aff1d14b2b6cb8
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-04-01 09:44:09 +0300

    Implement CommonMessageHandler

--- Application/Utils/CommonMessageHandler.cs
+++ Application/Utils/CommonMessageHandler.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Handlers.Interfaces;
+using Application.Services.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,35 @@ namespace Application.Utils
 {
     public class CommonMessageHandler
     {
+        private readonly IServiceProvider _provider;
+
+        public CommonMessageHandler(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public bool Handle(ICommand command)
+        {
+            Type type = typeof(ICommandHandler<>);
+            Type[] typeArgs = { command.GetType() };
+            Type handlerType = type.MakeGenericType(typeArgs);
+
+            dynamic? handler = _provider.GetService(handlerType);
+            bool result = handler!.Handle((dynamic)command);
+
+            return result;
+        }
+
+        public T Handle<T>(IQuery<T> query)
+        {
+            Type type = typeof(IQueryHandler<,>);
+            Type[] typeArgs = { query.GetType(), typeof(T) };
+            Type handlerType = type.MakeGenericType(typeArgs);
+
+            dynamic? handler = _provider.GetService(handlerType);
+            T result = handler!.Handle((dynamic)query);
+
+            return result;
+        }
     }
 }
