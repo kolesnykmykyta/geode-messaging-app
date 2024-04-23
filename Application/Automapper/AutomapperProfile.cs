4b09d6689f1aeed10ff8268dc385ef0167f95055
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-04-23 09:47:27 +0300

    Update namespace for Automapper

---
+++ Application/Automapper/AutomapperProfile.cs
@@ -0,0 +1,33 @@
+﻿using Application.Dtos;
+using Application.Services.Chats;
+using Application.Services.Messages;
+using Application.Services.Users;
+using AutoMapper;
+using DataAccess.Entities;
+
+namespace Application.Automapper
+{
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            // CQRS Messages
+            CreateMap<FilterDto, GetUsersListQuery>().ReverseMap();
+            CreateMap<FilterDto, GetUserChatsQuery>().ReverseMap();
+            CreateMap<FilterDto, GetUserMessagesQuery>().ReverseMap();
+            CreateMap<CreateChatCommand, Chat>().ReverseMap();
+            CreateMap<CreateChatCommand, ChatDto>().ReverseMap();
+            CreateMap<ChangeChatNameCommand, Chat>().ReverseMap();
+            CreateMap<ChangeChatNameCommand, ChatDto>().ReverseMap();
+            CreateMap<SendMessageCommand, Message>().ReverseMap();
+
+            // Entities to Dtos
+            CreateMap<UserInfoDto, User>().ReverseMap();
+            CreateMap<ChatDto, Chat>().ReverseMap();
+            CreateMap<MessageDto, Message>().ReverseMap();
+            CreateMap<Message, ChatMessageDto>()
+                .ForMember(cm => cm.Sender, m => m.MapFrom(x => x.Sender.UserName))
+                .ReverseMap();
+        }
+    }
+}
