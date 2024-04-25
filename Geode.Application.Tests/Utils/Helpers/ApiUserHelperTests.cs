a619b9170153cbd6831290e5b7938d274c49bed3
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-04-25 17:52:40 +0300

    Create tests for helpers

---
+++ Geode.Application.Tests/Utils/Helpers/ApiUserHelperTests.cs
@@ -0,0 +1,48 @@
+﻿using Application.Utils.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Geode.Application.Tests.Utils.Helpers
+{
+    public class ApiUserHelperTests
+    {
+        [Fact]
+        public void ExtractIdFromUser_UserWithId_ReturnsId()
+        {
+            string expected = "ClaimId";
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, expected)
+            };
+            ClaimsIdentity testIdentity = new ClaimsIdentity(claims);
+            ClaimsPrincipal testPrincipal = new ClaimsPrincipal(testIdentity);
+            ApiUserHelper sut = new ApiUserHelper();
+
+            string actual = sut.ExtractIdFromUser(testPrincipal);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ExtractNameFromUser_UserWithName_ReturnsName()
+        {
+            string expected = "ClaimName";
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, expected)
+            };
+            ClaimsIdentity testIdentity = new ClaimsIdentity(claims);
+            ClaimsPrincipal testPrincipal = new ClaimsPrincipal(testIdentity);
+            ApiUserHelper sut = new ApiUserHelper();
+
+            string actual = sut.ExtractNameFromUser(testPrincipal);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
---
+++ Geode.Application.Tests/Utils/Helpers/RepositoryParametersHelperTests.cs
@@ -0,0 +1,67 @@
+﻿using Application.Utils.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Geode.Application.Tests.Utils.Helpers
+{
+    public class RepositoryParametersHelperTests
+    {
+        [Theory]
+        [InlineData(" ")]
+        [InlineData(".")]
+        [InlineData("")]
+        [InlineData("Test")]
+        public void GenerateSearchParametersDictionary_AnyString_ReturnsDictionaryWithOnePair(string searchParam)
+        {
+            Dictionary<string, string> expected = new Dictionary<string, string>
+            {
+                {"all", searchParam},
+            };
+            RepositoryParametersHelper sut = new RepositoryParametersHelper();
+
+            Dictionary<string, string> actual = sut.GenerateSearchParametersDictionary(searchParam);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void GenerateSearchParametersDictionary_NullArgument_ReturnsEmptyDictionary()
+        {
+            Dictionary<string, string> expected = new Dictionary<string, string>();
+            RepositoryParametersHelper sut = new RepositoryParametersHelper();
+
+            Dictionary<string, string> actual = sut.GenerateSearchParametersDictionary(null);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData("test1,test2,test3")]
+        [InlineData("")]
+        [InlineData(",")]
+        [InlineData("nocomma")]
+        public void SplitSelectProperties_AnyString_SplitsStringByComma(string selectProps)
+        {
+            IEnumerable<string> expected = selectProps.Split(",");
+            RepositoryParametersHelper sut = new RepositoryParametersHelper();
+
+            IEnumerable<string> actual = sut.SplitSelectProperties(selectProps);
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SplitSelectProperties_NullArgument_ReturnsNull()
+        {
+            RepositoryParametersHelper sut = new RepositoryParametersHelper();
+
+            IEnumerable<string>? actual = sut.SplitSelectProperties(null);
+
+            Assert.Null(actual);
+        }
+    }
+}
