4df085d09d8a2c3754d161ce3e31cfd94812f8b0
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-04-26 18:26:01 +0300

    Add tests for the RefreshAsync method in the AuthService

--- Geode.Auth.Tests/Services/AuthServiceTests.cs
+++ Geode.Auth.Tests/Services/AuthServiceTests.cs
@@ -18,8 +18,10 @@ namespace Geode.Auth.Tests.Services
 {
     public class AuthServiceTests
     {
-        Mock<UserManager<User>> _userManagerMock;
-        Mock<IConfiguration> _configMock;
+        private readonly Mock<UserManager<User>> _userManagerMock;
+        private readonly Mock<IConfiguration> _configMock;
+
+        private const string testAccessToken = "eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9lbWFpbGFkZHJlc3MiOiJ0ZXN0QHRlc3QuY29tIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6InRlc3QiLCJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImJjMWU0ZWEwLTFhYjMtNDU2NC1hMWI3LWUyZTVmNmMwNzRkOCIsImV4cCI6MTcxNDE0Nzg1MywiaXNzIjoiVGVzdElzc3VlciIsImF1ZCI6IlRlc3RBdWRpZW5jZSJ9.IcV4HUHUf0La3hnP0pqeRV0zlZS4EZdemuB_yNQuqdA";
 
         public AuthServiceTests()
         {
@@ -150,11 +152,113 @@ namespace Geode.Auth.Tests.Services
             AuthService sut = new AuthService(_userManagerMock.Object, null);
 
             RegisterResultDto actual = await sut.RegisterAsync(new RegisterDto());
-            
+
             Assert.NotNull(actual.Errors);
             Assert.Equal(mockErrors.Count, actual.Errors.Count());
             Assert.Equal(mockErrors[0].Description, actual.Errors.ToList()[0]);
             Assert.Equal(mockErrors[1].Description, actual.Errors.ToList()[1]);
+        }
+
+        [Fact]
+        public async Task RefreshAsync_NoUserInToken_ReturnsNull()
+        {
+            PrepareConfigurationMock();
+            AuthService sut = new AuthService(_userManagerMock.Object, _configMock.Object);
+
+            TokenDto? actual = await sut.RefreshAsync(new TokenDto 
+            {
+                AccessToken = testAccessToken,
+            });
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public async Task RefreshAsync_UserRefreshTokenDiffers_ReturnsNull()
+        {
+            PrepareConfigurationMock();
+            _userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(new User { RefreshToken = "refresh1" });
+            AuthService sut = new AuthService(_userManagerMock.Object, _configMock.Object);
+
+            TokenDto? actual = await sut.RefreshAsync(new TokenDto 
+            {
+                AccessToken = testAccessToken,
+                RefreshToken = "refresh2"
+            });
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public async Task RefreshAsync_TokenIsExpired_ReturnsNull()
+        {
+            PrepareConfigurationMock();
+            _userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(new User { RefreshToken = "refresh1", RefreshTokenExpirationDate = DateTime.MinValue });
+            AuthService sut = new AuthService(_userManagerMock.Object, _configMock.Object);
+
+            TokenDto? actual = await sut.RefreshAsync(new TokenDto
+            {
+                AccessToken = testAccessToken,
+                RefreshToken = "refresh1"
+            });
+
+            Assert.Null(actual);
+        }
+
+        [Fact]
+        public async Task RefreshAsync_ValidRequest_ReturnsNewTokens()
+        {
+            PrepareConfigurationMock();
+            _userManagerMock.Setup(x => x.GetRolesAsync(It.IsAny<User>()))
+                .ReturnsAsync(new List<string>());
+            _userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(new User
+                {
+                    RefreshToken = "refresh1",
+                    RefreshTokenExpirationDate = DateTime.Now.AddDays(1),
+                    Email = "test",
+                    UserName = "test",
+                    Id = "test",
+                });
+            AuthService sut = new AuthService(_userManagerMock.Object, _configMock.Object);
+
+            TokenDto? actual = await sut.RefreshAsync(new TokenDto
+            {
+                AccessToken = testAccessToken,
+                RefreshToken = "refresh1"
+            });
+
+            Assert.NotNull(actual);
+            Assert.NotNull(actual.AccessToken);
+            Assert.NotNull(actual.RefreshToken);
+        }
+
+        [Fact]
+        public async Task RefreshAsync_ValidRequest_UpdatesRefreshToken()
+        {
+            PrepareConfigurationMock();
+            _userManagerMock.Setup(x => x.GetRolesAsync(It.IsAny<User>()))
+                .ReturnsAsync(new List<string>());
+            _userManagerMock.Setup(x => x.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(new User
+                {
+                    RefreshToken = "refresh1",
+                    RefreshTokenExpirationDate = DateTime.Now.AddDays(1),
+                    Email = "test",
+                    UserName = "test",
+                    Id = "test",
+                });
+            AuthService sut = new AuthService(_userManagerMock.Object, _configMock.Object);
+
+            _ = await sut.RefreshAsync(new TokenDto
+            {
+                AccessToken = testAccessToken,
+                RefreshToken = "refresh1"
+            });
+
+            _userManagerMock.Verify(x => x.UpdateAsync(It.IsAny<User>()), Times.Once);
         }
 
         private void PrepareMocksForLoginTest()
@@ -166,6 +270,11 @@ namespace Geode.Auth.Tests.Services
             _userManagerMock.Setup(x => x.GetRolesAsync(It.IsAny<User>()))
                 .ReturnsAsync(new List<string>());
 
+            PrepareConfigurationMock();
+        }
+
+        private void PrepareConfigurationMock()
+        {
             _configMock.Setup(x => x.GetSection("Jwt:Key").Value)
                 .Returns("TestKeyTestKeyTestKeyTestKeyTestKey");
             _configMock.Setup(x => x.GetSection("Jwt:Issuer").Value)
