a0260cc526cbbc96eea86f143a8a291f2b11fc1b
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-05-03 16:05:11 +0300

    Create tests for ChatHub

---
+++ Geode.Api.Tests/Hubs/ChatHubTests.cs
@@ -0,0 +1,58 @@
+﻿using Application.Services.Messages;
+using Application.Utils.Helpers.Interfaces;
+using Geode.API.Hubs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.SignalR;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Geode.Api.Tests.Hubs
+{
+    public class ChatHubTests
+    {
+        private const string ConnectionId = "connId";
+
+        private readonly Mock<IGroupManager> _groupManagerMock;
+        private readonly Mock<HubCallerContext> _contextMock;
+        private readonly Mock<IApiUserHelper> _apiUserHelperMock;
+        private readonly Mock<IMediator> _meditorMock;
+
+        public ChatHubTests()
+        {
+            _groupManagerMock = new Mock<IGroupManager>();
+
+            _contextMock = new Mock<HubCallerContext>();
+            _contextMock.SetupGet(x => x.ConnectionId)
+                .Returns(ConnectionId);
+
+            _apiUserHelperMock = new Mock<IApiUserHelper>();
+            _apiUserHelperMock.Setup(x => x.ExtractIdFromUser(It.IsAny<ClaimsPrincipal>()))
+                .Returns("testId");
+            _apiUserHelperMock.Setup(x => x.ExtractNameFromUser(It.IsAny<ClaimsPrincipal>()))
+                .Returns("testName");
+
+            _meditorMock = new Mock<IMediator>();
+        }
+
+        [Fact]
+        public async Task JoinChat_AddsUserToExpectedGroup()
+        {
+            ChatHub sut = new ChatHub(_apiUserHelperMock.Object, _meditorMock.Object)
+            {
+                Groups = _groupManagerMock.Object,
+                Context = _contextMock.Object,
+            };
+
+            await sut.JoinChat(1);
+
+            _groupManagerMock.Verify(x => x.AddToGroupAsync(ConnectionId, "chat-1", It.IsAny<CancellationToken>()), Times.Once);
+        }
+    }
+}
