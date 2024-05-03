738350235591f8a519290dfc2565ccc996f928a7
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-05-03 18:47:22 +0300

    Add tests for the ChatHub

--- Geode.Api.Tests/Hubs/ChatHubTests.cs
+++ Geode.Api.Tests/Hubs/ChatHubTests.cs
@@ -20,6 +20,8 @@ namespace Geode.Api.Tests.Hubs
         private const string ConnectionId = "connId";
 
         private readonly Mock<IGroupManager> _groupManagerMock;
+        private readonly Mock<IHubCallerClients> _clientsMock;
+        private readonly Mock<IClientProxy> _clientProxyMock;
         private readonly Mock<HubCallerContext> _contextMock;
         private readonly Mock<IApiUserHelper> _apiUserHelperMock;
         private readonly Mock<IMediator> _meditorMock;
@@ -28,9 +30,15 @@ namespace Geode.Api.Tests.Hubs
         {
             _groupManagerMock = new Mock<IGroupManager>();
 
+            _clientsMock = new Mock<IHubCallerClients>();
+
+            _clientProxyMock = new Mock<IClientProxy>();
+
             _contextMock = new Mock<HubCallerContext>();
             _contextMock.SetupGet(x => x.ConnectionId)
                 .Returns(ConnectionId);
+            _contextMock.SetupGet(x => x.User)
+                .Returns((ClaimsPrincipal)null);
 
             _apiUserHelperMock = new Mock<IApiUserHelper>();
             _apiUserHelperMock.Setup(x => x.ExtractIdFromUser(It.IsAny<ClaimsPrincipal>()))
@@ -53,6 +61,44 @@ namespace Geode.Api.Tests.Hubs
             await sut.JoinChat(1);
 
             _groupManagerMock.Verify(x => x.AddToGroupAsync(ConnectionId, "chat-1", It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task SendMessage_PassesExcutionToMeditor()
+        {
+            ChatHub sut = new ChatHub(_apiUserHelperMock.Object, _meditorMock.Object)
+            {
+                Groups = _groupManagerMock.Object,
+                Context = _contextMock.Object,
+                Clients = _clientsMock.Object,
+            };
+
+            _clientsMock.Setup(x => x.Group("chat-1"))
+                .Returns(_clientProxyMock.Object);
+
+            await sut.SendMessage(1, "test");
+
+            _meditorMock.Verify(x => x.Send(It.IsAny<SendMessageCommand>(), It.IsAny<CancellationToken>()), Times.Once);
+        }
+
+        [Fact]
+        public async Task SendMessage_NotifiesClients()
+        {
+            string message = "test";
+            ChatHub sut = new ChatHub(_apiUserHelperMock.Object, _meditorMock.Object)
+            {
+                Groups = _groupManagerMock.Object,
+                Context = _contextMock.Object,
+                Clients = _clientsMock.Object,
+            };
+
+            _clientsMock.Setup(x => x.Group("chat-1"))
+                .Returns(_clientProxyMock.Object);
+
+            await sut.SendMessage(1, message);
+
+            _clientProxyMock.Verify
+                (x => x.SendCoreAsync(It.IsAny<string>(), It.Is<object[]>(x => x.Contains(message)), It.IsAny<CancellationToken>()), Times.Once);
         }
     }
 }
