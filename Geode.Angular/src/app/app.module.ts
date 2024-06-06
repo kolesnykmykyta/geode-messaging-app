e0663a2ef75e5a50de15d97b833b99f116672c61
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-06-06 15:30:41 +0300

    Add auth module

--- Geode.Angular/src/app/app.module.ts
+++ Geode.Angular/src/app/app.module.ts
@@ -1,35 +1,29 @@
 import { NgModule } from '@angular/core';
 import { AgGridAngular } from 'ag-grid-angular'
 
-import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './auth/components/register/register.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-import { LoginComponent } from './auth/components/login/login.component';
 import { UsersComponent } from './users/users.component';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
     NavbarComponent,
-    LoginComponent,
     UsersComponent,
   ],
   imports: [
-    CommonModule,
     RouterOutlet,
-    FormsModule,
     HttpClientModule,
     BrowserModule,
     AppRoutingModule,
     AgGridAngular,
+    AuthModule,
   ],
   bootstrap: [AppComponent]
 })
