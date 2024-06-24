cfed2046b8e1f113c43b405a85f2abece754cfa3
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-06-24 17:01:12 +0300

    Use built-in method to convert numbers to the country format

--- Geode.Angular/src/app/shared/pipes/country-number.pipe.ts
+++ Geode.Angular/src/app/shared/pipes/country-number.pipe.ts
@@ -6,37 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class CountryNumberPipe implements PipeTransform {
   transform(value: number, countryCode: string): string {
-    let numberAsString = value.toString();
-    let [integerPart = '', decimalPart = ''] = numberAsString.split('.');
-    let integerTriplets = [];
-
-    for (let i = integerPart.length; i > 0; i -= 3) {
-      let start = Math.max(0, i - 3);
-      integerTriplets.unshift(integerPart.slice(start, i));
-    }
-
-    switch (countryCode) {
-      case 'US':
-      case 'UK':
-      case 'JP':
-        return this.joinAllNumberParts(integerTriplets, decimalPart, ',', '.');
-      case 'FR':
-        return this.joinAllNumberParts(integerTriplets, decimalPart, ' ', ',');
-      case 'DE':
-        return this.joinAllNumberParts(integerTriplets, decimalPart, '.', ',');
-      default:
-        return 'ERR_UNKNOWN_COUNTRY_CODE';
-    }
-  }
-
-  private joinAllNumberParts(
-    intTriplets: string[],
-    decimalPart: string,
-    tripletsDivider: string,
-    partsDivider: string
-  ) {
-    let integerPart = intTriplets.join(tripletsDivider);
-    decimalPart = decimalPart ? partsDivider + decimalPart : '';
-    return integerPart + decimalPart;
+    return value.toLocaleString(countryCode);
   }
 }
