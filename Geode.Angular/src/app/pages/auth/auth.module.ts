6e1ebdfe9c6b599d4b65070ff3f6cdd55e537164
Mykyta Kolesnyk <kolesnik23215@gmail.com>
2024-06-24 17:42:22 +0300

    Create DateFormatter pipe

--- Geode.Angular/src/app/pages/auth/auth.module.ts
+++ Geode.Angular/src/app/pages/auth/auth.module.ts
@@ -6,15 +6,10 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { SharedModule } from '../../shared/shared.module';
 import { ErrorHandlerPipe } from '../../shared/pipes/error-handler.pipe';
 import { CountryNumberPipe } from '../../shared/pipes/country-number.pipe';
+import { DateFormatterPipe } from '../../shared/pipes/date-formatter.pipe';
 
 @NgModule({
   declarations: [RegisterComponent, LoginComponent],
-  imports: [
-    CommonModule,
-    AuthRoutingModule,
-    SharedModule,
-    ErrorHandlerPipe,
-    CountryNumberPipe,
-  ],
+  imports: [CommonModule, AuthRoutingModule, SharedModule, ErrorHandlerPipe],
 })
 export class AuthModule {}
---
+++ Geode.Angular/src/app/shared/pipes/date-formatter.pipe.ts
@@ -0,0 +1,11 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'dateFormatter',
+  standalone: true,
+})
+export class DateFormatterPipe implements PipeTransform {
+  transform(value: Date, countryCode: string): string {
+    return value.toLocaleDateString(countryCode);
+  }
+}
